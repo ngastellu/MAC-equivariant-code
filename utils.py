b71294708b37860be1ae1ffa39dd637c8eeadce8
Nico <nicolas.gastellu@mail.mcgill.ca>
2025-06-18 15:22:31 -0400

    Added `weights_only=False` to `torch.load` in `utils.load_checkpoint_training`

--- utils.py
+++ utils.py
@@ -156,7 +156,7 @@ def load_checkpoint_training(configs, run_dir, model, optim):
     else: #negative start_epoch loads most recent checkpoint
         latest_epoch = max_epoch(configs.experiment_name)
         chk_path = run_dir / f'model-epoch_{latest_epoch}.pt'
-        checkpoint = torch.load(chk_path, map_location=device)
+        checkpoint = torch.load(chk_path, map_location=device, weights_only=False)
     
     model.load_state_dict(checkpoint['model_state_dict'])
     optim.load_state_dict(checkpoint['optimizer_state_dict'])
