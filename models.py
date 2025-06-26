8fa29cf057e5e94158600c5bd126fca727ce2fcb
Nico <nicolas.gastellu@mail.mcgill.ca>
2025-06-26 16:05:24 -0400

    Removed distinction between `EquivariantMaskedConv2d_90` and `EquivariantMaskedConv2d_180`; now keep all filters in the `nrot=4` case.

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ class MaskedConv2d(nn.Conv2d):
         return F.conv2d(x, self.weight * self.mask, self.bias, self.stride, self.padding)
 
 
-class EquivariantMaskedConv2d_180(nn.Module):
+class EquivariantMaskedConv2d(nn.Module):
     def __init__(self, mask_type, in_type, out_type, kernel_size, stride=1, padding=1, bias=False):
         super().__init__()
         assert mask_type in ['A', 'B'], "Mask type must be 'A' or 'B'"
@@ -60,72 +60,6 @@ class EquivariantMaskedConv2d_180(nn.Module):
     def export(self):
         # Export the internal R2Conv layer
         return self.conv.export()
-
-
-class EquivariantMaskedConv2d_90(nn.Module):
-    def __init__(self, mask_type, in_type, out_type, kernel_size, stride=1, padding=1, bias=False,filters=1):
-        super().__init__()
-        assert mask_type in ['A', 'B'], "Mask type must be 'A' or 'B'"
-        self.filters=filters
-        self.conv = e2nn.R2Conv(in_type, out_type, kernel_size, stride=stride, padding=padding, bias=bias)
-        self.mask_type = mask_type
-        # Register mask as a buffer
-        mask = torch.ones(1, 1, kernel_size, kernel_size)
-        _, _, h, w = mask.shape
-        mask[:, :, h // 2, w // 2 + (mask_type == 'B'):] = 0  # Block the center & future pixels
-        mask[:, :, h // 2 + 1:, :] = 0  # Block lower pixels
-        self.r2_act = gspaces.Rot2dOnR2(N=4)
-        self.register_buffer('mask', mask)
-
-    def forward(self, input):
-        print('***** In EquivariantMaskedConv2d_90.forward *****',flush=True)
-        # Expand the weights to get the convolution kernels
-        expanded_weights, expanded_bias = self.conv.expand_parameters()
-        # print(f'expanded_bias.shape = {expanded_bias.shape}', flush=True)
-
-        # Apply the mask to the expanded weights
-        # print(expanded_weights.shape)
-        expanded_weights1 = expanded_weights[:self.filters]
-        expanded_weights2 = expanded_weights[self.filters:int(self.filters*2), :]
-
-        if expanded_bias != None:
-            expanded_bias1 = expanded_bias[:self.filters]
-            expanded_bias2 = expanded_bias[self.filters:self.filters*2]
-        else:
-            expanded_bias1 = expanded_bias
-            expanded_bias2 = expanded_bias
-        masked_weights = expanded_weights1 * self.mask
-        masked_weights2 = expanded_weights2 * self.mask
-        #   print([self.mask_type,masked_weights])
-        output1 = nn.functional.conv2d(
-            input,
-            masked_weights,  # Apply mask to the first 20 filters
-            bias=expanded_bias1,
-            stride=self.conv.stride,
-            padding=self.conv.padding,
-            dilation=self.conv.dilation,
-            groups=self.conv.groups,
-        )
-
-        output2 = nn.functional.conv2d(
-            input,
-            masked_weights2,  # Apply mask to the second 20 filters
-            bias=expanded_bias2,
-            stride=self.conv.stride,
-            padding=self.conv.padding,
-            dilation=self.conv.dilation,
-            groups=self.conv.groups,
-        )
-        # Concatenate the outputs
-        output = torch.cat((output1, output2), dim=1)  # Concatenate along the channel dimension (dim=1)
-        reduced_out_type = e2nn.FieldType(self.r2_act, self.filters*2 * [self.r2_act.trivial_repr])
-        return e2nn.GeometricTensor(output, reduced_out_type)
-
-    def export(self):
-        # Export the internal R2Conv layer
-
-        return self.conv.export()
-
 
 
 class Activation(nn.Module):
@@ -169,28 +103,18 @@ class EquivariantPixelCNN(nn.Module):
         if self.init_layer_type == 'vanilla':
             self.initial_conv = MaskedConv2d('A', channels ,self.filters * 2, kernel_size, padding=padding, bias=True)
         else:
-            if self.nrot == 2:
-                self.initial_conv = EquivariantMaskedConv2d_180('A', self.input_type, self.hidden_type, kernel_size, padding=padding,bias=False)
-            else:
-                self.initial_conv = EquivariantMaskedConv2d_90('A', self.input_type, self.hidden_type, kernel_size, padding=padding,bias=False,filters=self.filters)
+            self.initial_conv = EquivariantMaskedConv2d('A', self.input_type, self.hidden_type, kernel_size, padding=padding,bias=False)
 
         # Hidden masked convolutions (Type 'B')
         self.vanilla_layers = nn.ModuleList([MaskedConv2d('B', self.filters*2, self.filters*2, kernel_size, padding=padding, bias=True)
                                             for _ in range(self.nb_vanilla_layers)
         ])
 
-        if self.nrot == 2:
-            self.equivariant_layers = nn.ModuleList([
-                EquivariantMaskedConv2d_180('B', self.hidden_type, self.hidden_type, kernel_size, padding=padding,bias=True)
+        self.equivariant_layers = nn.ModuleList([
+                EquivariantMaskedConv2d('B', self.hidden_type, self.hidden_type, kernel_size, padding=padding,bias=True)
                 for _ in range(self.nb_equivariant_layers)
             ])
-        else: # nrot = 4
-            self.hidden_type2 = e2nn.FieldType(self.r2_act, int(self.filters/2) * [self.r2_act.regular_repr])
-            self.equivariant_layers = nn.ModuleList([
-                EquivariantMaskedConv2d_90('B', self.hidden_type2, self.hidden_type, kernel_size, padding=padding,bias=True, filters=self.filters)
-                for _ in range(self.nb_equivariant_layers)
-            ])
-
+        
         if configs.fc_norm is None:
             self.fc_norm = nn.Identity()
         elif configs.fc_norm == 'batch':
